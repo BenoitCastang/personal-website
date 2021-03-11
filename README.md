2c0e6c59b46335944e70e415dd75ee93f663f35d
BenoitCastang <castang.benoit@gmail.com>
2021-03-11 08:16:37 +0100

    up

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # PortFolio
 
 This is a training website, meant to make me improve my skills.
-Next step is making it responsive and online.
-Next step is mastering CSS.
--- js/forms.js
+++ js/forms.js
@@ -1,8 +1,16 @@
-const para = document.getElementById('player-one');
-console.log(para);
+const para = document.getElementById('visitorName');
+const legend = document.querySelector('legend')
 para.addEventListener('click', updateName);
+para.addEventListener('keyIsDown(17)', updateName);
 
 function updateName() {
-  let name = prompt('Enter name');
-  para.textContent = 'Bienvenue à toi, ' + name + ' !';
+  let name = prompt('Quel est votre nom ?', 'Alice');
+  if(name != null || name != '') {
+    para.textContent = `Bienvenue, ${name} !`;
+    legend.textContent = `Aimez-vous mon site, ${name} ?`;
+  }
+  if(name == null || name == '') {
+    para.textContent = `Bienvenue !`;
+    legend.textContent = `Aimez-vous mon site ?`;
+  }
 }
